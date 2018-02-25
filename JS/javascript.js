1587de3a372e3a2e43fa5b162a5c32f208c2e6b3
Liam Docherty <liam_docherty@outlook.com>
2018-02-25 01:00:37 +0000

    commit

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,7 +2,4 @@ jQuery(document).ready(function($) {
     $('.type-it').typeIt({
         content: 'Hi, my name is Liam Docherty'
     });
-    $('.type-it2').typeIt({
-        content: 'I am a Front End developer based in the UK.If you are a business seeking a web presence or an employer looking to hire, you can get in touch with me'
-    });
 });
